ad410654f9c28ba3cd18829a5ad8961871c243b1
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2024-07-03 13:27:32 +0200

    Reduced code, Improved performances.

--- convert_PS3_outfit_2Take1.py
+++ convert_PS3_outfit_2Take1.py
@@ -3,8 +3,9 @@ from pathlib import Path
 
 
 class IndexItem:
-    def __init__(self, type: str, component_id: int, official_game_file__help_name: str, two_take_one__feature__help_name: str, notyourdope_rage_asm__code_comment__help_name: str):
+    def __init__(self, type: str, sorted_insert_pos: int, component_id: int, official_game_file__help_name: str, two_take_one__feature__help_name: str, notyourdope_rage_asm__code_comment__help_name: str):
         self.type = type
+        self.sorted_insert_pos = sorted_insert_pos
         self.component_id = component_id
         self.official_game_file__help_name = official_game_file__help_name
         self.two_take_one__feature__help_name = two_take_one__feature__help_name
@@ -13,120 +14,110 @@ class IndexItem:
 
 # Map each component and property to their corresponding line index in 2Take1 outfit .ini files.
 INDEX_MAP = {
-   7:   IndexItem('[COMPONENTS]',           0,   'head',  'Head',       'face'),
-   17:  IndexItem('[COMPONENTS]',           5,   'hand',  'Parachute',  'hands'),
-   9:   IndexItem('[COMPONENTS]',           1,   'berd',  'Mask',       'head'),
-   15:  IndexItem('[COMPONENTS]',           4,   'lowr',  'Legs',       'legs'),
-   11:  IndexItem('[COMPONENTS]',           2,   'hair',  'Hair',       'hair'),
-   21:  IndexItem('[COMPONENTS]',           7,   'teef',  'Accessory',  'special 1'),
-   13:  IndexItem('[COMPONENTS]',           3,   'uppr',  'Gloves',     'torso'),
-   29:  IndexItem('[COMPONENTS]',           11,  'jbib',  'Torso',      'torso 2'),
-   19:  IndexItem('[COMPONENTS]',           6,   'feet',  'Feet',       'shoes'),
-   23:  IndexItem('[COMPONENTS]',           8,   'accs',  'Torso 2',    'special 2'),
-   25:  IndexItem('[COMPONENTS]',           9,   'task',  'Vest',       'special 3'),
-   27:  IndexItem('[COMPONENTS]',           10,  'decl',  'Decal',      'textures'),
-   8:   IndexItem('[COMPONENTS_TEXTURES]',  0,   'head',  'Head',       'face texture'),
-   18:  IndexItem('[COMPONENTS_TEXTURES]',  5,   'hand',  'Parachute',  'hands texture'),
-   10:  IndexItem('[COMPONENTS_TEXTURES]',  1,   'berd',  'Mask',       'head texture'),
-   22:  IndexItem('[COMPONENTS_TEXTURES]',  7,   'teef',  'Accessory',  'special 1 texture'),
-   16:  IndexItem('[COMPONENTS_TEXTURES]',  4,   'lowr',  'Legs',       'legs texture'),
-   12:  IndexItem('[COMPONENTS_TEXTURES]',  2,   'hair',  'Hair',       'hair texture'),
-   14:  IndexItem('[COMPONENTS_TEXTURES]',  3,   'uppr',  'Gloves',     'torso texture'),
-   20:  IndexItem('[COMPONENTS_TEXTURES]',  6,   'feet',  'Feet',       'shoes texture'),
-   24:  IndexItem('[COMPONENTS_TEXTURES]',  8,   'accs',  'Torso 2',    'special 2 texture'),
-   26:  IndexItem('[COMPONENTS_TEXTURES]',  9,   'task',  'Vest',       'special 3 texture'),
-   28:  IndexItem('[COMPONENTS_TEXTURES]',  10,  'decl',  'Decal',      'textures texture'),
-   30:  IndexItem('[COMPONENTS_TEXTURES]',  11,  'jbib',  'Torso',      'torso 2 texture'),
-   1:   IndexItem('[PROPERTIES]',           0,   'head',  'Hat',        'hat'),
-   3:   IndexItem('[PROPERTIES]',           1,   'eyes',  'Glasses',    'glasses'),
-   5:   IndexItem('[PROPERTIES]',           2,   'ears',  'Ears',       'ear pieces'),
-   2:   IndexItem('[PROPERTIES_TEXTURES]',  0,   'head',  'Hat',        'hat texture'),
-   6:   IndexItem('[PROPERTIES_TEXTURES]',  2,   'ears',  'Ears',       'ear pieces texture'),
-   4:   IndexItem('[PROPERTIES_TEXTURES]',  1,   'eyes',  'Glasses',    'glasses texture'),
+   7:   IndexItem('[COMPONENTS]',           0,   0,   'head',  'Head',       'face'),
+   17:  IndexItem('[COMPONENTS]',           1,   5,   'hand',  'Parachute',  'hands'),
+   9:   IndexItem('[COMPONENTS]',           2,   1,   'berd',  'Mask',       'head'),
+   15:  IndexItem('[COMPONENTS]',           3,   4,   'lowr',  'Legs',       'legs'),
+   11:  IndexItem('[COMPONENTS]',           4,   2,   'hair',  'Hair',       'hair'),
+   21:  IndexItem('[COMPONENTS]',           5,   7,   'teef',  'Accessory',  'special 1'),
+   13:  IndexItem('[COMPONENTS]',           6,   3,   'uppr',  'Gloves',     'torso'),
+   29:  IndexItem('[COMPONENTS]',           7,   11,  'jbib',  'Torso',      'torso 2'),
+   19:  IndexItem('[COMPONENTS]',           8,   6,   'feet',  'Feet',       'shoes'),
+   23:  IndexItem('[COMPONENTS]',           9,  8,   'accs',  'Torso 2',    'special 2'),
+   25:  IndexItem('[COMPONENTS]',           10,  9,   'task',  'Vest',       'special 3'),
+   27:  IndexItem('[COMPONENTS]',           11,  10,  'decl',  'Decal',      'textures'),
+   8:   IndexItem('[COMPONENTS_TEXTURES]',  0,   0,   'head',  'Head',       'face texture'),
+   18:  IndexItem('[COMPONENTS_TEXTURES]',  1,   5,   'hand',  'Parachute',  'hands texture'),
+   10:  IndexItem('[COMPONENTS_TEXTURES]',  2,   1,   'berd',  'Mask',       'head texture'),
+   22:  IndexItem('[COMPONENTS_TEXTURES]',  3,   7,   'teef',  'Accessory',  'special 1 texture'),
+   16:  IndexItem('[COMPONENTS_TEXTURES]',  4,   4,   'lowr',  'Legs',       'legs texture'),
+   12:  IndexItem('[COMPONENTS_TEXTURES]',  5,   2,   'hair',  'Hair',       'hair texture'),
+   14:  IndexItem('[COMPONENTS_TEXTURES]',  6,   3,   'uppr',  'Gloves',     'torso texture'),
+   20:  IndexItem('[COMPONENTS_TEXTURES]',  7,   6,   'feet',  'Feet',       'shoes texture'),
+   24:  IndexItem('[COMPONENTS_TEXTURES]',  8,   8,   'accs',  'Torso 2',    'special 2 texture'),
+   26:  IndexItem('[COMPONENTS_TEXTURES]',  9,  9,   'task',  'Vest',       'special 3 texture'),
+   28:  IndexItem('[COMPONENTS_TEXTURES]',  10,  10,  'decl',  'Decal',      'textures texture'),
+   30:  IndexItem('[COMPONENTS_TEXTURES]',  11,  11,  'jbib',  'Torso',      'torso 2 texture'),
+   1:   IndexItem('[PROPERTIES]',           0,   0,   'head',  'Hat',        'hat'),
+   3:   IndexItem('[PROPERTIES]',           1,   1,   'eyes',  'Glasses',    'glasses'),
+   5:   IndexItem('[PROPERTIES]',           2,   2,   'ears',  'Ears',       'ear pieces'),
+   2:   IndexItem('[PROPERTIES_TEXTURES]',  0,   0,   'head',  'Hat',        'hat texture'),
+   6:   IndexItem('[PROPERTIES_TEXTURES]',  1,   2,   'ears',  'Ears',       'ear pieces texture'),
+   4:   IndexItem('[PROPERTIES_TEXTURES]',  2,   1,   'eyes',  'Glasses',    'glasses texture'),
 }
 # User settings START
 #MODLOADER_PATH = Path('D:/Downloads/GTA Stuff/PS3/2) MODLOADERS/BUZZARD v6.1 Private/source by JR/ModLoader.csa')
 MODLOADER_PATH = Path('D:/Downloads/GTA Stuff/PS3/2) MODLOADERS/BUZZARD v6.1 Private/source by JR/BUZZARD_6_1_male_outfits.csa')
 #MODLOADER_PATH = Path('D:/Downloads/GTA Stuff/PS3/2) MODLOADERS/BUZZARD v6.1 Private/source by JR/BUZZARD_6_1_female_outfits.csa')
-FILENAME_MALE_OR_FEMALE = "male" # male / female / mixed
+FILENAME_MALE_OR_FEMALE = 'male' # male / female / mixed
 FILENAME_SANITIZER = True
 # User settings END
+RE_FILENAME_SANITIZER_PATTERN = re.compile('|'.join(map(re.escape, ['~b~', '~n~', '~p~', '~r~', ':'])))
 RE_OUTFIT_PATTERN = re.compile(
     r'^:(?P<outfit_label>[\w&\.-]+)(?: ?//.*)?$\n(?P<outfit_data>(?:Push1? (?:-1|\d{1,3})(?: ?//.*)?$(?:\n)){30})Call @[\w&\.-]+$(?:\nPushString "(?P<outfit_name>[^"]+)")?',
     re.MULTILINE
 )
 RE_OUTFIT_PUSH_VALUE_PATTERN = re.compile(r'^Push1? (?P<int_value>-1|\d{1,3})$')
-INVALID_WINDOWS_FILENAME_CHARS = set("\\/:*?\"<>|")
+INVALID_WINDOWS_FILENAME_CHARS = set('\\/:*?"<>|')
 
 
 def convert_invalid_windows_filename_chars_to_unicode(filename: str):
-    return ''.join(f"U+{ord(char):04X}" if char in INVALID_WINDOWS_FILENAME_CHARS else char for char in filename)
+    return ''.join(f'U+{ord(char):04X}' if char in INVALID_WINDOWS_FILENAME_CHARS else char for char in filename)
 
-def outfit_converter(outfit_int_values_list: list[int]):
-    categories: dict[str, list[str]] = {
-        '[COMPONENTS]': [],
-        '[COMPONENTS_TEXTURES]': [],
-        '[PROPERTIES]': [],
-        '[PROPERTIES_TEXTURES]': [],
+def outfit_converter():
+    outfit_categories: dict[str, list[str]] = {
+        '[COMPONENTS]': [''] * 12,
+        '[COMPONENTS_TEXTURES]': [''] * 12,
+        '[PROPERTIES]': [''] * 3,
+        '[PROPERTIES_TEXTURES]': [''] * 3,
     }
 
     for i, int_value in enumerate(outfit_int_values_list, start=1):
         map_item = INDEX_MAP[i]
 
-        categories[map_item.type].append(f'index{map_item.component_id}={int_value} ;; {map_item.two_take_one__feature__help_name}')
-
-    sorted_categories: dict[str, list[str]] = {
-        '[COMPONENTS]': [],
-        '[COMPONENTS_TEXTURES]': [],
-        '[PROPERTIES]': [],
-        '[PROPERTIES_TEXTURES]': [],
-    }
-
-    for map_item2 in INDEX_MAP.values():
-        for category in sorted_categories:
-            if map_item2.type == category:
-                for line in categories[category]:
-                    if line.startswith(f'index{map_item2.component_id}='):
-                        sorted_categories[category].append(line)
+        outfit_categories[map_item.type][map_item.sorted_insert_pos] = f'index{map_item.component_id}={int_value} ;; {map_item.two_take_one__feature__help_name}'
 
     return '\n'.join(
         f'{category}\n' +
-        '\n'.join(sorted_categories[category])
-        for category in sorted_categories
+        '\n'.join(outfit_categories[category])
+        for category in outfit_categories
     ).removesuffix('\n')
+
+def generate_outfit_filename():
+    outfit_path = Path(f'PS3_{FILENAME_MALE_OR_FEMALE}_{str(i).zfill(max_generated_outfits__index_length)}.ini')
+
+    sanitized_outfit_name = None
+
+    if outfit_name:
+        if FILENAME_SANITIZER:
+            sanitized_outfit_name = RE_FILENAME_SANITIZER_PATTERN.sub('', outfit_name)
+        else:
+            sanitized_outfit_name = outfit_name
+    elif outfit_label:
+        sanitized_outfit_name = outfit_label
+
+    if sanitized_outfit_name:
+        sanitized_outfit_name = convert_invalid_windows_filename_chars_to_unicode(sanitized_outfit_name)
+        outfit_path = outfit_path.with_name(f'{outfit_path.stem}_{sanitized_outfit_name}').with_suffix(outfit_path.suffix)
+
+    return outfit_path
 
 
 matches = list(RE_OUTFIT_PATTERN.finditer(MODLOADER_PATH.read_text(encoding='utf-8')))
-max_index_length = len(str(len(matches)))
+max_generated_outfits__index_length = len(str(len(matches)))
 
 for i, match in enumerate(matches, start=1):
     outfit_label = match['outfit_label']
     outfit_data = match['outfit_data']
     outfit_name = match['outfit_name']
 
-    outfit_int_values_list = []
+    outfit_int_values_list: list[int] = []
     for data in outfit_data.splitlines(keepends=False):
         match = RE_OUTFIT_PUSH_VALUE_PATTERN.search(data)
         outfit_int_values_list.append(int(match['int_value']))
 
-    translated_output_data = outfit_converter(outfit_int_values_list)
+    translated_outfit_data = outfit_converter()
+    outfit_path = generate_outfit_filename()
 
-    padded_index = str(i).zfill(max_index_length) # Zero-pad the outfit index
-
-    outfit_filename = Path(f'PS3_outfit_{FILENAME_MALE_OR_FEMALE}_{padded_index}.ini')
-
-    base_name_to_sanitize = None
-    if outfit_name:
-        base_name_to_sanitize = outfit_name
-        if FILENAME_SANITIZER:
-            base_name_to_sanitize = base_name_to_sanitize.replace("~b~", "").replace("~n~", "").replace("~p~", "").replace("~r~", "").replace(":", "")
-    elif outfit_label:
-        base_name_to_sanitize = outfit_label
-
-    if base_name_to_sanitize:
-        sanitized_base_name = convert_invalid_windows_filename_chars_to_unicode(base_name_to_sanitize)
-        outfit_filename = outfit_filename.with_name(f'{outfit_filename.stem}_{sanitized_base_name}').with_suffix(outfit_filename.suffix)
-
-    outfit_filename.write_text(translated_output_data, encoding='utf-8')
-
-    print(f'Created outfit: "{outfit_filename}"')
+    outfit_path.write_text(translated_outfit_data, encoding='utf-8')
+    print(f'Created outfit: "{outfit_path}"')
